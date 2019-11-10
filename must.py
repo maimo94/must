aab76771d5f416068031f9e02c597b58f456c782
Mai Mohammed <mai94mohammed@gmail.com>
2019-11-11 00:18:09 +0200

    SQL Statment to check DB

--- must.py
+++ must.py
@@ -1,1 +1,15 @@
-print("hello")
+import mysql.connector
+
+db =  mysql.connector.connect(host="localhost", # Host, usually localhost
+                     user="root", # your username
+                     password="mai123", # your password
+                     db="must_student") # name of the data base
+#create a Cursor object.
+cur = db.cursor()
+
+# Write SQL statement here
+cur.execute("SELECT * FROM pharmacy  ;")
+
+# print all the first and second cells of all the rows
+for row in cur.fetchall() :
+    print (row[0])
